a443700a28d5585f2521a9562dd8ed02dbf080af
Erik Winge <erik.winge@ub.uio.no>
2016-12-20 11:58:58 +0100

    make service transactional

--- src/main/java/no/uio/ub/neo/service/impl/DefaultConceptService.java
+++ src/main/java/no/uio/ub/neo/service/impl/DefaultConceptService.java
@@ -4,11 +4,13 @@ import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import no.uio.ub.neo.dao.ConceptDAO;
 import no.uio.ub.neo.model.Concept;
 import no.uio.ub.neo.service.ConceptService;
 
+@Transactional
 @Component("conceptService")
 public class DefaultConceptService implements ConceptService {
     @Autowired
