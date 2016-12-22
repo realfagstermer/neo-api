b17e0c79825da066ac9300d2987203781fc88575
Erik Winge <erik.winge@ub.uio.no>
2016-12-22 12:41:03 +0100

    format code

--- src/main/java/no/uio/ub/neo/model/Concept.java
+++ src/main/java/no/uio/ub/neo/model/Concept.java
@@ -8,218 +8,217 @@ import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import java.sql.Timestamp;
 import java.util.List;
 
-
 /**
  * The persistent class for the concepts database table.
  *
  */
 @Entity
-@Table(name="concepts")
-@NamedQuery(name="Concept.findAll", query="SELECT c FROM Concept c")
-public class Concept  {
+@Table(name = "concepts")
+@NamedQuery(name = "Concept.findAll", query = "SELECT c FROM Concept c")
+public class Concept {
 
-	@Id
-	@SequenceGenerator(name="CONCEPTS_CONCEPTID_GENERATOR", sequenceName="CONCEPTS_CONCEPT_ID_SEQ")
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="CONCEPTS_CONCEPTID_GENERATOR")
-	@Column(name="concept_id")
-	private Integer conceptId;
+    @Id
+    @SequenceGenerator(name = "CONCEPTS_CONCEPTID_GENERATOR", sequenceName = "CONCEPTS_CONCEPT_ID_SEQ")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "CONCEPTS_CONCEPTID_GENERATOR")
+    @Column(name = "concept_id")
+    private Integer conceptId;
 
-	@Column(name="concept_type")
-	private String conceptType;
+    @Column(name = "concept_type")
+    private String conceptType;
 
-	private Timestamp created;
+    private Timestamp created;
 
-	@Column(name="created_by")
-	private Integer createdBy;
+    @Column(name = "created_by")
+    private Integer createdBy;
 
-	private String definition;
+    private String definition;
 
-	private Timestamp deprecated;
+    private Timestamp deprecated;
 
-	@Column(name="deprecated_by")
-	private Integer deprecatedBy;
+    @Column(name = "deprecated_by")
+    private Integer deprecatedBy;
 
-	@Column(name="editorial_note")
-	private String editorialNote;
+    @Column(name = "editorial_note")
+    private String editorialNote;
 
-	@Column(name="external_id")
-	private Integer externalId;
+    @Column(name = "external_id")
+    private Integer externalId;
 
-	private Timestamp modified;
+    private Timestamp modified;
 
-	@Column(name="modified_by")
-	private Integer modifiedBy;
+    @Column(name = "modified_by")
+    private Integer modifiedBy;
 
-	private String note;
+    private String note;
 
-	@Column(name="replaced_by")
-	private Integer replacedBy;
+    @Column(name = "replaced_by")
+    private Integer replacedBy;
 
-	@Column(name="scope_note")
-	private String scopeNote;
+    @Column(name = "scope_note")
+    private String scopeNote;
 
-	@Column(name="used_by_libs")
-	private String usedByLibs;
+    @Column(name = "used_by_libs")
+    private String usedByLibs;
 
-	@Column(name="vocab_id")
-	private String vocabId;
+    @Column(name = "vocab_id")
+    private String vocabId;
 
-	//bi-directional many-to-one association to Term
-	@OneToMany(mappedBy="concept", fetch=FetchType.EAGER)
-	@JsonIdentityInfo(generator=ObjectIdGenerators.IntSequenceGenerator.class, property="@id")
-	private List<Term> terms;
+    // bi-directional many-to-one association to Term
+    @OneToMany(mappedBy = "concept", fetch = FetchType.EAGER)
+    @JsonIdentityInfo(generator = ObjectIdGenerators.IntSequenceGenerator.class, property = "@id")
+    private List<Term> terms;
 
-	public Concept() {
-	}
+    public Concept() {
+    }
 
-	public Integer getConceptId() {
-		return this.conceptId;
-	}
+    public Integer getConceptId() {
+        return this.conceptId;
+    }
 
-	public void setConceptId(Integer conceptId) {
-		this.conceptId = conceptId;
-	}
+    public void setConceptId(Integer conceptId) {
+        this.conceptId = conceptId;
+    }
 
-	public String getConceptType() {
-		return this.conceptType;
-	}
+    public String getConceptType() {
+        return this.conceptType;
+    }
 
-	public void setConceptType(String conceptType) {
-		this.conceptType = conceptType;
-	}
+    public void setConceptType(String conceptType) {
+        this.conceptType = conceptType;
+    }
 
-	public Timestamp getCreated() {
-		return this.created;
-	}
+    public Timestamp getCreated() {
+        return this.created;
+    }
 
-	public void setCreated(Timestamp created) {
-		this.created = created;
-	}
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
 
-	public Integer getCreatedBy() {
-		return this.createdBy;
-	}
+    public Integer getCreatedBy() {
+        return this.createdBy;
+    }
 
-	public void setCreatedBy(Integer createdBy) {
-		this.createdBy = createdBy;
-	}
+    public void setCreatedBy(Integer createdBy) {
+        this.createdBy = createdBy;
+    }
 
-	public String getDefinition() {
-		return this.definition;
-	}
+    public String getDefinition() {
+        return this.definition;
+    }
 
-	public void setDefinition(String definition) {
-		this.definition = definition;
-	}
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
 
-	public Timestamp getDeprecated() {
-		return this.deprecated;
-	}
+    public Timestamp getDeprecated() {
+        return this.deprecated;
+    }
 
-	public void setDeprecated(Timestamp deprecated) {
-		this.deprecated = deprecated;
-	}
+    public void setDeprecated(Timestamp deprecated) {
+        this.deprecated = deprecated;
+    }
 
-	public Integer getDeprecatedBy() {
-		return this.deprecatedBy;
-	}
+    public Integer getDeprecatedBy() {
+        return this.deprecatedBy;
+    }
 
-	public void setDeprecatedBy(Integer deprecatedBy) {
-		this.deprecatedBy = deprecatedBy;
-	}
+    public void setDeprecatedBy(Integer deprecatedBy) {
+        this.deprecatedBy = deprecatedBy;
+    }
 
-	public String getEditorialNote() {
-		return this.editorialNote;
-	}
+    public String getEditorialNote() {
+        return this.editorialNote;
+    }
 
-	public void setEditorialNote(String editorialNote) {
-		this.editorialNote = editorialNote;
-	}
+    public void setEditorialNote(String editorialNote) {
+        this.editorialNote = editorialNote;
+    }
 
-	public Integer getExternalId() {
-		return this.externalId;
-	}
+    public Integer getExternalId() {
+        return this.externalId;
+    }
 
-	public void setExternalId(Integer externalId) {
-		this.externalId = externalId;
-	}
+    public void setExternalId(Integer externalId) {
+        this.externalId = externalId;
+    }
 
-	public Timestamp getModified() {
-		return this.modified;
-	}
+    public Timestamp getModified() {
+        return this.modified;
+    }
 
-	public void setModified(Timestamp modified) {
-		this.modified = modified;
-	}
+    public void setModified(Timestamp modified) {
+        this.modified = modified;
+    }
 
-	public Integer getModifiedBy() {
-		return this.modifiedBy;
-	}
+    public Integer getModifiedBy() {
+        return this.modifiedBy;
+    }
 
-	public void setModifiedBy(Integer modifiedBy) {
-		this.modifiedBy = modifiedBy;
-	}
+    public void setModifiedBy(Integer modifiedBy) {
+        this.modifiedBy = modifiedBy;
+    }
 
-	public String getNote() {
-		return this.note;
-	}
+    public String getNote() {
+        return this.note;
+    }
 
-	public void setNote(String note) {
-		this.note = note;
-	}
+    public void setNote(String note) {
+        this.note = note;
+    }
 
-	public Integer getReplacedBy() {
-		return this.replacedBy;
-	}
+    public Integer getReplacedBy() {
+        return this.replacedBy;
+    }
 
-	public void setReplacedBy(Integer replacedBy) {
-		this.replacedBy = replacedBy;
-	}
+    public void setReplacedBy(Integer replacedBy) {
+        this.replacedBy = replacedBy;
+    }
 
-	public String getScopeNote() {
-		return this.scopeNote;
-	}
+    public String getScopeNote() {
+        return this.scopeNote;
+    }
 
-	public void setScopeNote(String scopeNote) {
-		this.scopeNote = scopeNote;
-	}
+    public void setScopeNote(String scopeNote) {
+        this.scopeNote = scopeNote;
+    }
 
-	public String getUsedByLibs() {
-		return this.usedByLibs;
-	}
+    public String getUsedByLibs() {
+        return this.usedByLibs;
+    }
 
-	public void setUsedByLibs(String usedByLibs) {
-		this.usedByLibs = usedByLibs;
-	}
+    public void setUsedByLibs(String usedByLibs) {
+        this.usedByLibs = usedByLibs;
+    }
 
-	public String getVocabId() {
-		return this.vocabId;
-	}
+    public String getVocabId() {
+        return this.vocabId;
+    }
 
-	public void setVocabId(String vocabId) {
-		this.vocabId = vocabId;
-	}
+    public void setVocabId(String vocabId) {
+        this.vocabId = vocabId;
+    }
 
-	public List<Term> getTerms() {
-		return this.terms;
-	}
+    public List<Term> getTerms() {
+        return this.terms;
+    }
 
-	public void setTerms(List<Term> terms) {
-		this.terms = terms;
-	}
+    public void setTerms(List<Term> terms) {
+        this.terms = terms;
+    }
 
-	public Term addTerm(Term term) {
-		getTerms().add(term);
-		term.setConcept(this);
+    public Term addTerm(Term term) {
+        getTerms().add(term);
+        term.setConcept(this);
 
-		return term;
-	}
+        return term;
+    }
 
-	public Term removeTerm(Term term) {
-		getTerms().remove(term);
-		term.setConcept(null);
+    public Term removeTerm(Term term) {
+        getTerms().remove(term);
+        term.setConcept(null);
 
-		return term;
-	}
+        return term;
+    }
 
 }
--- src/main/java/no/uio/ub/neo/model/Term.java
+++ src/main/java/no/uio/ub/neo/model/Term.java
@@ -6,122 +6,121 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.sql.Timestamp;
 
-
 /**
  * The persistent class for the terms database table.
  *
  */
 @Entity
-@Table(name="terms")
-@NamedQuery(name="Term.findAll", query="SELECT t FROM Term t")
-public class Term  {
-	private static final long serialVersionUID = 1L;
+@Table(name = "terms")
+@NamedQuery(name = "Term.findAll", query = "SELECT t FROM Term t")
+public class Term {
+    private static final long serialVersionUID = 1L;
 
-	@Id
-	@SequenceGenerator(name="TERMS_TERMID_GENERATOR", sequenceName="TERMS_TERM_ID_SEQ")
-	@GeneratedValue(strategy=GenerationType.SEQUENCE, generator="TERMS_TERMID_GENERATOR")
-	@Column(name="term_id")
-	private Integer termId;
+    @Id
+    @SequenceGenerator(name = "TERMS_TERMID_GENERATOR", sequenceName = "TERMS_TERM_ID_SEQ")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "TERMS_TERMID_GENERATOR")
+    @Column(name = "term_id")
+    private Integer termId;
 
-	@Column(name="lang_id")
-	private String langId;
+    @Column(name = "lang_id")
+    private String langId;
 
-	@Column(name="lexical_value")
-	private String lexicalValue;
+    @Column(name = "lexical_value")
+    private String lexicalValue;
 
-	private String status;
+    private String status;
 
-	@Column(name="term_created")
-	private Timestamp termCreated;
+    @Column(name = "term_created")
+    private Timestamp termCreated;
 
-	@Column(name="term_created_by")
-	private Integer termCreatedBy;
+    @Column(name = "term_created_by")
+    private Integer termCreatedBy;
 
-	@Column(name="term_modified")
-	private Timestamp termModified;
+    @Column(name = "term_modified")
+    private Timestamp termModified;
 
-	@Column(name="term_modified_by")
-	private Integer termModifiedBy;
+    @Column(name = "term_modified_by")
+    private Integer termModifiedBy;
 
-	//bi-directional many-to-one association to Concept
-	@JsonIgnore
-	@ManyToOne(fetch=FetchType.LAZY)
-	@JoinColumn(name="concept_id")
-	private Concept concept;
+    // bi-directional many-to-one association to Concept
+    @JsonIgnore
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "concept_id")
+    private Concept concept;
 
-	public Term() {
-	}
+    public Term() {
+    }
 
-	public Integer getTermId() {
-		return this.termId;
-	}
+    public Integer getTermId() {
+        return this.termId;
+    }
 
-	public void setTermId(Integer termId) {
-		this.termId = termId;
-	}
+    public void setTermId(Integer termId) {
+        this.termId = termId;
+    }
 
-	public String getLangId() {
-		return this.langId;
-	}
+    public String getLangId() {
+        return this.langId;
+    }
 
-	public void setLangId(String langId) {
-		this.langId = langId;
-	}
+    public void setLangId(String langId) {
+        this.langId = langId;
+    }
 
-	public String getLexicalValue() {
-		return this.lexicalValue;
-	}
+    public String getLexicalValue() {
+        return this.lexicalValue;
+    }
 
-	public void setLexicalValue(String lexicalValue) {
-		this.lexicalValue = lexicalValue;
-	}
+    public void setLexicalValue(String lexicalValue) {
+        this.lexicalValue = lexicalValue;
+    }
 
-	public String getStatus() {
-		return this.status;
-	}
+    public String getStatus() {
+        return this.status;
+    }
 
-	public void setStatus(String status) {
-		this.status = status;
-	}
+    public void setStatus(String status) {
+        this.status = status;
+    }
 
-	public Timestamp getTermCreated() {
-		return this.termCreated;
-	}
+    public Timestamp getTermCreated() {
+        return this.termCreated;
+    }
 
-	public void setTermCreated(Timestamp termCreated) {
-		this.termCreated = termCreated;
-	}
+    public void setTermCreated(Timestamp termCreated) {
+        this.termCreated = termCreated;
+    }
 
-	public Integer getTermCreatedBy() {
-		return this.termCreatedBy;
-	}
+    public Integer getTermCreatedBy() {
+        return this.termCreatedBy;
+    }
 
-	public void setTermCreatedBy(Integer termCreatedBy) {
-		this.termCreatedBy = termCreatedBy;
-	}
+    public void setTermCreatedBy(Integer termCreatedBy) {
+        this.termCreatedBy = termCreatedBy;
+    }
 
-	public Timestamp getTermModified() {
-		return this.termModified;
-	}
+    public Timestamp getTermModified() {
+        return this.termModified;
+    }
 
-	public void setTermModified(Timestamp termModified) {
-		this.termModified = termModified;
-	}
+    public void setTermModified(Timestamp termModified) {
+        this.termModified = termModified;
+    }
 
-	public Integer getTermModifiedBy() {
-		return this.termModifiedBy;
-	}
+    public Integer getTermModifiedBy() {
+        return this.termModifiedBy;
+    }
 
-	public void setTermModifiedBy(Integer termModifiedBy) {
-		this.termModifiedBy = termModifiedBy;
-	}
+    public void setTermModifiedBy(Integer termModifiedBy) {
+        this.termModifiedBy = termModifiedBy;
+    }
 
-	public Concept getConcept() {
-		return this.concept;
-	}
+    public Concept getConcept() {
+        return this.concept;
+    }
 
-	public void setConcept(Concept concept) {
-		this.concept = concept;
-	}
+    public void setConcept(Concept concept) {
+        this.concept = concept;
+    }
 
 }
